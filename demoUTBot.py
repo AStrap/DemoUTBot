a38bd7701235780476fa1ceaf76b426771c89226
alex.strappazzon <alex.strappazzon@bft-automation.com>
2024-04-01 17:10:52 +0200

    add some commands and management of file

--- demoUTBot.py
+++ demoUTBot.py
@@ -1,13 +1,14 @@
 import telebot
 import time
 import pandas as pd
+from telebot import types
 
+# - login
 def loadCSV():
     df = pd.read_csv('login.csv', dtype=str)
     keys = list(df['NAME'].dropna())
     values = list(df['ID'].dropna())
     map_values = dict(zip(keys, values))
-    print(map_values)
     return map_values
 
 bot = telebot.TeleBot('7068784385:AAG1QR74-WqHVvg8JuN_MLBzkRfvnXlervo')
@@ -44,8 +45,51 @@ def send_welcome(message):
         else:
             reply = "You can't use the bot"
     else:
-        reply = "Please insert your name after /start"
+        reply = "Please insert your name after /start (/start <name>)"
     bot.reply_to(message, reply)
+# --
+
+# - commands
+@bot.message_handler(commands=['hi'])
+def send_welcome(message):
+    if str(message.chat.id) in users_map.values():
+        reply = "Hi"
+    else:
+        reply = "You can't use the bot or you need to login (/start <name>)"
+    bot.reply_to(message, reply)
+# --
+    
+# - doc
+@bot.message_handler(commands=['doc'])
+def send_welcome(message):
+    if str(message.chat.id) in users_map.values():
+        markup = types.ReplyKeyboardMarkup(row_width=1)
+        itembtn1 = types.KeyboardButton('/ADEL')
+        itembtn2 = types.KeyboardButton('/X56')
+        markup.add(itembtn1, itembtn2)
+        bot.send_message(message.chat.id, "What doc you need:", reply_markup=markup)
+    else:
+        reply = "You can't use the bot or you need to login (/start <name>)"
+        bot.reply_to(message, reply)
+
+@bot.message_handler(commands=['ADEL'])
+def send_welcome(message):
+    if str(message.chat.id) in users_map.values():
+        doc = open('documentation/ADEL.pdf', 'rb')
+        bot.send_document(message.chat.id, doc)
+    else:
+        reply = "You can't use the bot or you need to login (/start <name>)"
+        bot.reply_to(message, reply)
+
+@bot.message_handler(commands=['X56'])
+def send_welcome(message):
+    if str(message.chat.id) in users_map.values():
+        doc = open('documentation/X56.pdf', 'rb')
+        bot.send_document(message.chat.id, doc)
+    else:
+        reply = "You can't use the bot or you need to login (/start <name>)"
+        bot.reply_to(message, reply)
+# --
 
 bot.polling()
 
